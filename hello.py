b5fb12435dd4543db93b35ac7e01edd398927d24
mbenloucif <mbenlouc@emich.edu>
2021-11-08 03:50:11 +0000

    added hello.py and venv

---
+++ hello.py
@@ -0,0 +1,7 @@
+import emoji 
+import pyjokes 
+import numpy 
+
+print(emoji.emojize('python is :beaming_face_with_smiling_eyes:'))
+print(pyjokes.get_joke())
+print(numpy.random.randint(9, size=(3, 3)))
